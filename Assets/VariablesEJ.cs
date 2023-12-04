f8df75e1d08c018b99de1363ef7c64881557a3eb
GonzaloNog <63429016+GonzaloNog@users.noreply.github.com>
2023-12-04 12:53:49 -0300

    operaciones

--- Assets/VariablesEJ.cs
+++ Assets/VariablesEJ.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class VariablesEJ : MonoBehaviour
 {
     private int resultado = 0;
-    public int a = 0;
-    public int b = 0;
+    private int a = 0;
+    private int b = 0;
+
+    public TMP_InputField num1;
+    public TMP_InputField num2;
+    public TextMeshProUGUI resultadoText;
 
     public void plus()
     {
@@ -16,6 +21,8 @@ public class VariablesEJ : MonoBehaviour
     public void mensaje()
     {
         Debug.Log(resultado);
+        resultadoText.text = resultado.ToString();
+        //resultadoText.text = "HOLA";
     }
     public void resta()
     {
@@ -26,6 +33,12 @@ public class VariablesEJ : MonoBehaviour
     {
         resultado = (a * b);
         mensaje();
+    }
+
+    public void convertInput()
+    {
+        a = int.Parse(num1.text);
+        b = int.Parse(num2.text);
     }
 
 }
