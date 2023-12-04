0ef0e1091f76d7fa74f537e5739999a65645e7d6
GonzaloNog <63429016+GonzaloNog@users.noreply.github.com>
2023-12-04 12:07:16 -0300

    ejer2

---
+++ Assets/VariablesEJ.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesEJ : MonoBehaviour
+{
+    private string mensaje = "hola";
+    public string mensaje2 = "hola2";
+    void Start()
+    {
+        Debug.Log(mensaje);
+        Debug.Log(mensaje2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
